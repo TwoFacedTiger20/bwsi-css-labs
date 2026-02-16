2ec4d3e9b598eb3e117e6092b10347665a09eb2c
TwoFacedTiger20 <avaneeshreddyd@gmail.com>
2026-02-16 13:22:24 -0800

    Add user input check

--- labs/lab_1/lab_1b.py
+++ labs/lab_1/lab_1b.py
@@ -37,13 +37,29 @@ def simple_calculator(operation: str, num1: float, num2: float) -> float:
     else:
         raise ValueError("Invalid operation. Please choose from 'add', 'subtract', 'multiply', or 'divide'.")
 
+
+def request_santized_numer(prompt: str):
+    """
+    Requests santized numbers for calculation
+
+    returns:
+        Float: santized inpuit by user
+    """
+    while True:
+        try:
+            number = float(input(prompt))
+            return number
+        except:
+            print("Invalid Input. Enter Valid Number")
+
+
 def main():
     
     print(f"===== Simple Calculator =====")
 
     # Ask the user for sample input    
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))
+    num1 = request_santized_numer("Enter the first number: ")
+    num2 = request_santized_numer("Enter the second number: ")
     operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
 
     # Perform the calculation and display the result
